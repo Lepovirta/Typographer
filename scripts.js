33df9a0ac78e1b81a1e3120d7dee4a7e0a3623a2
Esa Juhana Lahikainen <esa.lahikainen@descom.fi>
2016-01-30 11:54:10 +0200

    Add font locks, refactor to use knockout.js, remove unused fonts

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function spaceToPlus(string) {
 
 function randBetween(min, max) {
   return Math.floor(Math.random()*(max-min+1)+min);
-};
+}
 
 function Font(name, size) {
   this.name = name;
@@ -33,41 +33,70 @@ function selectOne(fontList) {
   return fontList[randomIndex];
 }
 
-function newFonts(fonts) {
+function newFonts(firstLocked, secondLocked, fonts) {
 
-  if(typeof clicky !== "undefined") {
-    clicky.log('#new-combination','New combination generated');
+  if(typeof clicky !== "undefined") { clicky.log('#new-combination','New combination generated'); }
+  
+  if(!firstLocked) {
+    var headerFontName = selectOne(fonts);
+    var headerFontSize = randBetween(30, 70);
+    var headerFont = new Font(headerFontName, headerFontSize);
+    headerFont.useStyleFor('h1');
+    headerFont.useStyleFor('h2');
+    $('#header-font').attr('href', headerFont.nameAsLinkElement());
+    $('#header-font').text(headerFont.name);
   }
-
-  var headerFontName = selectOne(fonts);
-  var headerFontSize = randBetween(30, 70);
-  var headerFont = new Font(headerFontName, headerFontSize);
-  headerFont.useStyleFor('h1');
-  headerFont.useStyleFor('h2');
-  $('#header-font').attr('href', headerFont.nameAsLinkElement());
-  $('#header-font').text(headerFont.name);
-
-  var contentFontName = selectOne(fonts);
-  var contentFontSize = randBetween(14, 20);
-  var contentFont = new Font(contentFontName, contentFontSize);
-  contentFont.useStyleFor('p');
-  $('#content-font').attr('href', contentFont.nameAsLinkElement());
-  $('#content-font').text(contentFont.name);
-
+  
+  if(!secondLocked) {
+    var contentFontName = selectOne(fonts);
+    var contentFontSize = randBetween(14, 20);
+    var contentFont = new Font(contentFontName, contentFontSize);
+    contentFont.useStyleFor('p');
+    $('#content-font').attr('href', contentFont.nameAsLinkElement());
+    $('#content-font').text(contentFont.name);
+  }
+  
 }
 
-$(document).ready(function(){
-  var fonts = Typographer.fontList;
-  newFonts(fonts);
+var fonts = Typographer.fontList;
+
+function booleanToEnglish(boolean) {
+  return boolean ? "Yes" : "No";
+}
+
+function ViewModel() {
+  var self = this;
+  
+  self.firstLocked = ko.observable(false);
+  self.secondLocked = ko.observable(false);
+
+  newFonts(self.firstLocked(), self.secondLocked(), fonts);
+  
+  self.firstLabel = ko.computed(function(){
+    return "Lock #1 ("+booleanToEnglish(self.firstLocked())+")";
+  });
+  
+  self.secondLabel = ko.computed(function(){
+    return "Lock #2 ("+booleanToEnglish(self.secondLocked())+")";
+  });
+
+  self.lockFirst = function() {
+    self.firstLocked(!self.firstLocked());
+  }
+
+  self.lockSecond = function() {
+    self.secondLocked(!self.secondLocked());
+  }
+
+  self.newCombination = function () {
+    newFonts(self.firstLocked(), self.secondLocked(), fonts);
+  };
 
   $(document).keypress(function(e) {
-    if(e.which == 32) { // Space
-      newFonts(fonts);
+    if(e.which == 32) {
+      newFonts(self.firstLocked(), self.secondLocked(), fonts);
     }
-  });
+  });    
+} 
 
-  $('#generate').on('click', function(){
-    newFonts(fonts);
-  });
-
-});
+ko.applyBindings(new ViewModel());
